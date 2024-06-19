86bccfd959dc14d51792c0c5f8550608d727bc5b
Kevin Lin <2004klin@gmail.com>
2024-06-18 21:53:00 -0600

    Add object counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import ObjectDetection from './ObjectDetection';
-
+import ObjectDetection from "./ObjectDetection";
 
 function App() {
   return (
     <div className="app">
       <h1>Image Object Detection</h1>
-        <ObjectDetection />
+      <ObjectDetection />
     </div>
   );
 }
--- src/ObjectDetection.jsx
+++ src/ObjectDetection.jsx
@@ -1,30 +1,30 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import * as cocoSsd from '@tensorflow-models/coco-ssd';
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
 
-import '@tensorflow/tfjs';
+import "@tensorflow/tfjs";
 
 const ObjectDetection = () => {
   const videoRef = useRef(null);
-  const [isWebcamStarted, setIsWebcamStarted] = useState(false)
+  const [isWebcamStarted, setIsWebcamStarted] = useState(false);
 
   const [predictions, setPredictions] = useState([]);
 
-  const [detectionInterval, setDetectionInterval] = useState()
+  const [detectionInterval, setDetectionInterval] = useState();
 
-
+  const [numOfDetections, setNumOfDetections] = useState(0);
 
   const startWebcam = async () => {
     try {
-      setIsWebcamStarted(true)
+      setIsWebcamStarted(true);
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
 
       if (videoRef.current) {
         videoRef.current.srcObject = stream;
       }
     } catch (error) {
-      setIsWebcamStarted(false)
-      console.error('Error accessing webcam:', error);
+      setIsWebcamStarted(false);
+      console.error("Error accessing webcam:", error);
     }
   };
 
@@ -40,77 +40,96 @@ const ObjectDetection = () => {
       });
 
       video.srcObject = null;
-      setPredictions([])
-      setIsWebcamStarted(false)
+      setPredictions([]);
+      setIsWebcamStarted(false);
+      setNumOfDetections(0);
     }
   };
 
   const predictObject = async () => {
     const model = await cocoSsd.load();
 
-    model.detect(videoRef.current).then((predictions) => {
-      setPredictions(predictions);
-    })
+    model
+      .detect(videoRef.current)
+      .then((predictions) => {
+        setPredictions(predictions);
+        setNumOfDetections(predictions.length);
+      })
 
-      .catch(err => {
-        console.error(err)
+      .catch((err) => {
+        console.error(err);
       });
   };
 
   useEffect(() => {
     if (isWebcamStarted) {
-      setDetectionInterval(setInterval(predictObject, 500))
+      setDetectionInterval(setInterval(predictObject, 500));
     } else {
       if (detectionInterval) {
-        clearInterval(detectionInterval)
-        setDetectionInterval(null)
+        clearInterval(detectionInterval);
+        setDetectionInterval(null);
       }
     }
-  }, [isWebcamStarted])
+  }, [isWebcamStarted]);
 
   return (
     <div className="object-detection">
       <div className="buttons">
-        <button onClick={isWebcamStarted ? stopWebcam : startWebcam}>{isWebcamStarted ? "Stop" : "Start"} Webcam</button>
+        <button onClick={isWebcamStarted ? stopWebcam : startWebcam}>
+          {isWebcamStarted ? "Stop" : "Start"} Webcam
+        </button>
       </div>
-      <div className="feed">
-        {isWebcamStarted ? <video ref={videoRef} autoPlay muted /> : <div />}
-        {/* Add the tags below to show a label using the p element and a box using the div element */}
-        {predictions.length > 0 && (
-          predictions.map(prediction => {
-            return <>
-              <p style={{
-                left: `${prediction.bbox[0]}px`, 
-                top: `${prediction.bbox[1]}px`,
-                width: `${prediction.bbox[2] - 100}px`
-            }}>{prediction.class  + ' - with ' 
-            + Math.round(parseFloat(prediction.score) * 100) 
-            + '% confidence.'}</p>
-            <div className={"marker"} style={{
-              left: `${prediction.bbox[0]}px`,
-              top: `${prediction.bbox[1]}px`,
-              width: `${prediction.bbox[2]}px`,
-              height: `${prediction.bbox[3]}px`
-            }} />
-            </>
-          })
-        )}
-      </div>
-      {/* Add the tags below to show a list of predictions to user */}
-      {predictions.length > 0 && (
-        <div>
-          <h3>Predictions:</h3>
-          <ul>
-            {predictions.map((prediction, index) => (
-              <li key={index}>
-                {`${prediction.class} (${(prediction.score * 100).toFixed(2)}%)`}
-              </li>
-            ))}
-          </ul>
+      <div style={{display:"flex", flexDirection:"row"}}>
+        <div className="feed">
+          {isWebcamStarted ? <video ref={videoRef} autoPlay muted /> : <div />}
+          {/* Add the tags below to show a label using the p element and a box using the div element */}
+          {predictions.length > 0 &&
+            predictions.map((prediction) => {
+              return (
+                <>
+                  <p
+                    style={{
+                      left: `${prediction.bbox[0]}px`,
+                      top: `${prediction.bbox[1]}px`,
+                      width: `${prediction.bbox[2] - 100}px`,
+                    }}
+                  >
+                    {prediction.class +
+                      " - with " +
+                      Math.round(parseFloat(prediction.score) * 100) +
+                      "% confidence."}
+                  </p>
+                  <div
+                    className={"marker"}
+                    style={{
+                      left: `${prediction.bbox[0]}px`,
+                      top: `${prediction.bbox[1]}px`,
+                      width: `${prediction.bbox[2]}px`,
+                      height: `${prediction.bbox[3]}px`,
+                    }}
+                  />
+                </>
+              );
+            })}
         </div>
-      )}
-
-    </div>
+        {/* Add the tags below to show a list of predictions to user */}
+        {predictions.length > 0 && (
+          <div>
+            <h3>Predictions:</h3>
+            <ul>
+              {predictions.map((prediction, index) => (
+                <li key={index}>
+                  {`${prediction.class} (${(prediction.score * 100).toFixed(
+                    2
+                  )}%)`}
+                </li>
+              ))}
+            </ul>
+            <p>Number of connections: {numOfDetections}</p>
+          </div>
+        )}
+            </div>
+      </div>
   );
 };
 
